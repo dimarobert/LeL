b726c2cea105041c22e18bc4f27eca761a773bfd
Dima Robert <dima.robert94@gmail.com>
2016-04-11 04:20:54 +0300

    Change site logo with the site name

--- config/assets/default.js
+++ config/assets/default.js
@@ -5,7 +5,8 @@ module.exports = {
     lib: {
       css: [
         'public/lib/bootstrap/dist/css/bootstrap.css',
-        'public/lib/bootstrap/dist/css/bootstrap-theme.css'
+        'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+        'public/lib/angular-material/angular-material.css'
       ],
       js: [
         'public/lib/angular/angular.js',
@@ -16,6 +17,8 @@ module.exports = {
         'public/lib/angular-ui-utils/ui-utils.js',
         'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
         'public/lib/angular-file-upload/angular-file-upload.js',
+        'public/lib/angular-aria/angular-aria.js',
+        'public/lib/angular-material/angular-material.js',
         'public/lib/owasp-password-strength-test/owasp-password-strength-test.js'
       ],
       tests: ['public/lib/angular-mocks/angular-mocks.js']
