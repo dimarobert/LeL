4dd2c513a837398049e2f49fcf991f5afbc919bd
Robert Dima <robert.dima@dnnsharp.com>
2016-05-27 23:15:07 +0300

    Live update top 15

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication', 'CoursesService', 'Top15Service',
-  function ($scope, Authentication, CoursesService, Top15Service) {
+angular.module('core').controller('HomeController', ['$scope', '$interval', 'Authentication', 'CoursesService', 'Top15Service',
+  function ($scope, $interval, Authentication, CoursesService, Top15Service) {
     // This provides Authentication context.
     $scope.authentication = Authentication;
 
@@ -11,37 +11,51 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 
     (function () {
       if ($scope.authentication.user) {
-        for (var course of $scope.authentication.user.startedCourses) {
-          CoursesService.get({ courseId: course.course }).$promise
-            .then(function (result) {
-              var old = $scope.userCourses.findIndex(function (uc) { return uc._id == result._id });
-              if (old > -1)
-                $scope.userCourses[old] = result;
-              else
-                $scope.userCourses.push(result);
-            });
+        updateTop15();
+        //start the timeout
+        var topUpdate = $interval(function () {
+          updateTop15();
+        }, 5000);
 
-          Top15Service.query({ courseId: course.course }).$promise
-            .then(function (result) {
-              var old = $scope.top15.findIndex(function (t) { return t.courseId == course.course; });
-              if (old > -1)
-                $scope.top15[old] = { courseId: course.course, result: result };
-              else
-                $scope.top15.push({ courseId: course.course, result: result });
-            });
-
-          Top15Service.get({ courseId: course.course, action: 'currentuserplace' }).$promise
-            .then(function (result) {
-              var old = $scope.userPlace.findIndex(function (up) { return up.courseId == course.course; });
-              if (old > -1)
-                $scope.userPlace[old] = { courseId: course.course, result: result };
-              else
-                $scope.userPlace.push({ courseId: course.course, result: result });
-            });
-        }
+        $scope.$on('$destroy', function () {
+          if (topUpdate) {
+            $interval.cancel(topUpdate);
+          }
+        });
       }
 
     })();
+
+    function updateTop15() {
+      for (var course of $scope.authentication.user.startedCourses) {
+        CoursesService.get({ courseId: course.course }).$promise
+          .then(function (result) {
+            var old = $scope.userCourses.findIndex(function (uc) { return uc._id == result._id });
+            if (old > -1)
+              $scope.userCourses[old] = result;
+            else
+              $scope.userCourses.push(result);
+          });
+
+        Top15Service.query({ courseId: course.course }).$promise
+          .then(function (result) {
+            var old = $scope.top15.findIndex(function (t) { return t.courseId == course.course; });
+            if (old > -1)
+              $scope.top15[old] = { courseId: course.course, result: result };
+            else
+              $scope.top15.push({ courseId: course.course, result: result });
+          });
+
+        Top15Service.get({ courseId: course.course, action: 'currentuserplace' }).$promise
+          .then(function (result) {
+            var old = $scope.userPlace.findIndex(function (up) { return up.courseId == course.course; });
+            if (old > -1)
+              $scope.userPlace[old] = { courseId: course.course, result: result };
+            else
+              $scope.userPlace.push({ courseId: course.course, result: result });
+          });
+      }
+    }
 
 
     $scope.getTop15 = function (course) {
